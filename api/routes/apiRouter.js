97bf453456b71fc500cc526015b0e178aafe50d3
LucasCruzTI <125907237+LucasCruzTI@users.noreply.github.com>
2023-11-20 18:25:01 -0300

    Últimas correções

--- api/routes/apiRouter.js
+++ api/routes/apiRouter.js
@@ -49,17 +49,141 @@ let isAdmin = (req, res, next) => {
           return
         }
         else {
-          res.status(403).json({ message: 'Role de ADMIN requerida' })
+          res.status(403).json({ message: 'Necessário ser ADMIN para prosseguir com a solicitação' })
           return
         }
       }
     })
     .catch(err => {
       res.status(500).json({
-        message: 'Erro ao verificar roles de usuário - ' + err.message
+        message: 'Erro ao consultar usuário - ' + err.message
       })
     })
 } 
+
+apiRouter.get(endpoint + 'category', checkToken, (req, res) => {
+
+  const {page = 1, pageSize = 10, sortBy = 'pid', sortOrder = 'asc', description} = req.query;
+  const offset = (page - 1) * pageSize;
+  
+
+  let query = knex.select('*').from('category');
+
+  // Aplicar filtro pelo nome, se fornecido
+  if (description) {
+
+      query.where('description', 'ilike', `%${description}%`);
+
+  }
+
+  // Aplicar ordenação
+  query.orderBy(sortBy, sortOrder);
+
+  // Aplicar paginação
+  query.limit(pageSize).offset(offset);
+
+  query.then(category => {
+    const result = {
+      data: category,
+      links: {
+        self: 'https://gerenciamentoprodutos-webapi-nodejs.com'+req.originalUrl, // Link para a lista atual
+      },
+    };
+
+    res.status(200).json(result);
+  }).catch(err => {
+    res.status(500).json({
+      message: 'Erro ao recuperar categorias - ' + err.message
+    });
+  });
+});
+
+apiRouter.get(endpoint + 'category/:id', checkToken, (req, res) => {
+  const categoryId = req.params.id;
+
+  knex.select('*')
+    .from('category')
+    .where('pid', categoryId)
+    .then(category => {
+      if (category.length > 0) {
+        const result = {
+          data: category[0],
+          links: {
+            self: 'https://gerenciamentoprodutos-webapi-nodejs.com'+req.originalUrl, // Link para o recurso atual
+            collection: 'https://gerenciamentoprodutos-webapi-nodejs.com/api/' + 'category', // Link para a coleção de categorias
+          },
+        };
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({ message: 'Categoria não encontrada' });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Erro ao processar a solicitação' });
+    });
+});
+
+apiRouter.post(endpoint + 'category', checkToken, isAdmin, (req, res) => {
+  const newCategory = req.body;
+
+  if (!newCategory) {
+    return res.status(400).json({ error: 'Os dados da categoria são obrigatórios' });
+  }
+
+  knex('category')
+    .insert(newCategory)
+    .returning('*')
+    .then(created => {
+      res.status(201).json(created[0]);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Erro ao processar a solicitação' });
+    });
+});
+
+apiRouter.put(endpoint + 'category/:id', checkToken, isAdmin, (req, res) => { 
+  const categoryId = req.params.id;
+  const updatedData = req.body;
+
+knex('category')
+  .where('pid', categoryId)
+  .update(updatedData)
+  .returning('*')  
+  .then(updated => {
+    if (updated.length > 0) {
+      res.status(200).json(updated[0]);
+    } else {
+      res.status(404).json({ message: 'Categoria não encontrada' });
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ erro: 'Erro ao processar a solicitação' });
+  });
+})
+
+apiRouter.delete(endpoint + 'category/:id', checkToken, isAdmin, (req, res) => { 
+  const categoryId = req.params.id;
+
+knex('category')
+  .where('pid', categoryId)
+  .del()
+  .returning('*')  
+  .then(excluded => {
+    if (excluded.length > 0) {
+      res.status(200).json(excluded[0]);
+    } else {
+      res.status(404).json({ message: 'Categoria não encontrada' });
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao processar a solicitação' });
+  });
+})
+
 
 apiRouter.get(endpoint + 'products', checkToken, (req, res) => {
 
@@ -141,9 +265,8 @@ apiRouter.post(endpoint + 'products', checkToken, isAdmin, (req, res) => {
         console.error(err);
         res.status(500).json({ error: 'Erro ao processar a solicitação' });
       });
-  });
+});
   
-
 apiRouter.put(endpoint + 'products/:id', checkToken, isAdmin, (req, res) => { 
     const productId = req.params.id;
   const updatedData = req.body;
